65dffde92fccdcb0d39149a4687696384ecd62e0
Kristaps Kaupe <kristaps@blogiem.lv>
2023-04-01 10:22:15 +0300

    Add script, template and readme

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
 # zbx-bitcoin
+
+Template and script for Zabbix to monitor Bitcoin Core or Knots node.
+
+## Dependencies
+
+* bitcoin-cli
+* jq
+* zabbix_sender
+
+## Setup
+
+* Import zbx_export_templates.yaml into Zabbix;
+* Make zbx-bitcoin-status.sh run on a machine continuously, either using SystemD or cron job (it has a locking mechanism, you can just launch it each minute from cron).
+
+## Items
+
+* Current number of headers validated
+* Estimated size of the block and undo files on disk
+* Estimate of blockchain verification progress
+* Height of the most-work fully-validated chain
+* Mempool size
+* Node connection count
+* Node incoming connection count
+* Node outgoing connection count
+* Number of active RPC commands
+* Protocol version
+* Server subversion string
+* Server version
+* Total memory usage for the mempool
+* TX count in mempool
+
+## Triggers
+
+* Protocol version has changed
+* Server subversion string has changed
+* Server version has changed
+* Validated blockchain tip hasn't changed for 3 hours
+
+## Dashboard
+
+![image](https://user-images.githubusercontent.com/4500994/229270913-573e89da-7624-4aba-914d-f505beb3b758.png)
---
+++ zbx-bitcoin-status.sh
@@ -0,0 +1,79 @@
+#!/usr/bin/env bash
+
+bitcoin_cli="bitcoin-cli"
+bitcoin_cli_options=""
+zabbix_sender="zabbix_sender -c /etc/zabbix/zabbix_agentd.conf"
+measurement_rate="60s"
+
+lockdir="/tmp/$(basename "$0").lock"
+if mkdir "$lockdir" 2> /dev/null; then
+    trap 'rm -rf "$lockdir"' 0
+    trap "exit 2" 1 2 3 15
+else
+    exit 1
+fi
+echo "$$" > "$lockdir"/pid 2> /dev/null || exit 2
+
+# assume all first parameters beginning with dash are bitcoin-cli options
+while (( ${#} > 0 )) && [[ ${1:0:1} == "-" ]]; do
+    bitcoin_cli_options="$bitcoin_cli_options $1"
+    shift
+done
+bitcoin_cli="$bitcoin_cli$bitcoin_cli_options"
+
+echo "[$(date -u +"%Y-%m-%dT%H:%M:%SZ")] $bitcoin_cli"
+
+while :; do
+
+    echo "[$(date -u +"%Y-%m-%dT%H:%M:%SZ")] Requesting bitcoind status"
+
+    blockchain_info="$($bitcoin_cli getblockchaininfo)"
+    if [[ -z "$blockchain_info" ]]; then
+        # empty response here means bitcoind is not responding, retry after 1 sec.
+        echo "[$(date -u +"%Y-%m-%dT%H:%M:%SZ")] ... empty response"
+        sleep 1s
+        continue
+    fi
+
+    blockchain_tip_blocks="$(jq ".blocks" <<< "$blockchain_info")"
+    blockchain_tip_headers="$(jq ".headers" <<< "$blockchain_info")"
+    blockchain_verificationprogress="$(jq ".verificationprogress" <<< "$blockchain_info")"
+    blockchain_size_on_disk="$(jq ".size_on_disk" <<< "$blockchain_info")"
+
+    network_info="$($bitcoin_cli getnetworkinfo)"
+    bitcoind_version="$(jq ".version" <<< "$network_info")"
+    bitcoind_subversion="$(jq ".subversion" <<< "$network_info")"
+    bitcoind_protocolversion="$(jq ".protocolversion" <<< "$network_info")"
+    bitcoind_connections="$(jq ".connections" <<< "$network_info")"
+    bitcoind_connections_in="$(jq ".connections_in" <<< "$network_info")"
+    bitcoind_connections_out="$(jq ".connections_out" <<< "$network_info")"
+
+    mempool_info="$($bitcoin_cli getmempoolinfo)"
+    mempool_tx_count="$(jq ".size" <<< "$mempool_info")"
+    mempool_size_vbytes="$(jq ".bytes" <<< "$mempool_info")"
+    mempool_usage_bytes="$(jq ".usage" <<< "$mempool_info")"
+
+    rpc_active_commands="$($bitcoin_cli getrpcinfo | jq ".active_commands | length")"
+    # -1, to not count us
+    rpc_active_commands=$(( rpc_active_commands - 1 ))
+
+    set -x
+    $zabbix_sender -k bitcoin.blockchain.tip.blocks -o $blockchain_tip_blocks
+    $zabbix_sender -k bitcoin.blockchain.tip.headers -o $blockchain_tip_headers
+    $zabbix_sender -k bitcoin.blockchain.verificationprogress -o $blockchain_verificationprogress
+    $zabbix_sender -k bitcoin.blockchain.size_on_disk -o $blockchain_size_on_disk
+    $zabbix_sender -k bitcoin.version -o $bitcoind_version
+    $zabbix_sender -k bitcoin.subversion -o "$bitcoind_subversion"
+    $zabbix_sender -k bitcoin.protocolversion -o $bitcoind_protocolversion
+    $zabbix_sender -k bitcoin.connections.num -o $bitcoind_connections
+    $zabbix_sender -k bitcoin.connections_in.num -o $bitcoind_connections_in
+    $zabbix_sender -k bitcoin.connections_out.num -o $bitcoind_connections_out
+    $zabbix_sender -k bitcoin.mempool.tx_count -o $mempool_tx_count
+    $zabbix_sender -k bitcoin.mempool.size_vbytes -o $mempool_size_vbytes
+    $zabbix_sender -k bitcoin.mempool.usage_bytes -o $mempool_usage_bytes
+    $zabbix_sender -k bitcoin.rpc.active_commands.num -o $rpc_active_commands
+    set +x
+
+    echo "[$(date -u +"%Y-%m-%dT%H:%M:%SZ")] Sleeping for $measurement_rate"
+    sleep "$measurement_rate"
+done
